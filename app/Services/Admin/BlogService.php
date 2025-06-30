aa3864596f7ee595ee8260ba8c3daf14b4a4865e
TakayukiHirano117 <ikusa.hirano@icloud.com>
2025-06-30 14:34:49 +0900

    更新用メソッドのプロトタイプをService, Repositoryに実装

--- app/Services/Admin/BlogService.php
+++ app/Services/Admin/BlogService.php
@@ -3,20 +3,55 @@
 namespace App\Services\Admin;
 
 use App\Repositories\Admin\BlogRepository;
+use App\Models\Blog;
+use Illuminate\Database\Eloquent\Collection;
+use Storage;
 
 class BlogService
 {
-    public function __construct(protected BlogRepository $blogRepository) {}
+    public function __construct(protected BlogRepository $blogRepository)
+    {
+    }
+
+    /**
+     * ブログを全件取得する
+     */
+    public function getAllBlog(): Collection
+    {
+        return $this->blogRepository->getAllBlog();
+    }
+
+    /**
+     * ブログ１件の詳細データを取得する
+     */
+    public function getBlog($id): Blog
+    {
+        return $this->blogRepository->getBlog($id);
+    }
 
     /**
      * ブログを登録する
-     * @param array $blogRequestData
-     * @return void
      */
     public function storeBlog(array $blogRequestData): void
     {
         $savedImagePath = $blogRequestData['image']->store('blogs', 'public');
         $blogRequestData['image'] = $savedImagePath;
         $this->blogRepository->store($blogRequestData);
+    }
+
+    /**
+     * ブログを更新する
+     */
+    public function updateBlog($request, $id): void
+    {
+        $blog = $this->blogRepository->getBlog($id);
+        $validatedRequestData = $request->validated();
+
+        if($request->hasFile('image')) {
+            Storage::disk('public')->delete($blog->image);
+            $validatedRequestData['image'] = $request->file('image')->store('blogs', 'public');
+        }
+
+        $this->blogRepository->update($blog, $validatedRequestData);
     }
 }
